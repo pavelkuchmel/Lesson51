19145e43ecbddc696216ba764ae9d186149ad58d
Pavel Koutchmel <zarkonmen@gmail.com>
2023-03-15 15:48:29 +0300

    1

--- Homework.js
+++ Homework.js
@@ -5,24 +5,26 @@ let deg = document.getElementById("deg");
 const bulletStartingX = bullet.getClientRects()[0].left;
 let bulletCurrentX = bullet.getClientRects()[0].left;
 let move = false;
-/*let deg = document.createElement('p');
-document.appendChild(deg);*/
+let mouseDown = false;
+
+/*
+let deg = document.createElement('p');
+document.appendChild(deg);
+*/
 
 console.log('clientWidth: ' + document.body.clientWidth + ' clientHeight: ' + document.body.clientHeight);
+console.log('offsetWidth: ' + document.body.offsetWidth + ' clientHeight: ' + document.body.offsetHeight);
 let clientRects = document.body.getClientRects();
 console.log(clientRects);
-/*for (let i = 0; i < clientRects.length; i++){
-    console.log(clientRects[i]);
-}*/
 
-aim.style.top = document.body.clientHeight / 2 - 25 + 'px';
-aim.style.left = document.body.clientWidth - 80 - 25 + 'px';
+aim.style.top = document.body.offsetHeight / 2 - 25 + 'px';
+aim.style.left = document.body.offsetWidth - 80 - 25 + 'px';
 
-gun.style.top = document.body.clientHeight / 2 - 15 + 'px';
+gun.style.top = document.body.offsetHeight / 2 - 15 + 'px';
 
-bullet.style.top = document.body.clientHeight / 2 - 13 + 'px';
+bullet.style.top = document.body.offsetHeight / 2 - 13 + 'px';
 
-function moveBullet(){
+function moveBullet(mouseX, mouseY){
     if (move) {
         bulletCurrentX ++;
         if (bulletCurrentX >= aim.getClientRects()[0].left) {
@@ -36,11 +38,12 @@ function moveBullet(){
     }
 };
 
-gun.addEventListener("click", function (){
+addEventListener("click", function (event){
     if (!move) {
         move = true;
-        moveBullet();
+        moveBullet(event.pageX, event.pageY);
     }
+
 });
 
 addEventListener("keydown", function (event){
@@ -57,20 +60,38 @@ addEventListener("keydown", function (event){
             move = false;
         }
     }
-})
+});
 
-addEventListener("mousemove", function (event){
-    let x = event.pageX;
-    let y;
-    if (event.pageY > document.body.clientHeight / 2){
-        y = event.pageY - document.body.clientHeight / 2;
+addEventListener('mousedown', function (){
+    mouseDown = true;
+});
+
+addEventListener('mouseup', function (){
+    mouseDown = false;
+});
+
+document.body.addEventListener("mousemove", function (event){
+    if (mouseDown){
+        let x = event.pageX;
+        let y;
+        if (event.pageY > document.body.offsetHeight / 2) {
+            y = event.pageY - document.body.offsetHeight / 2;
+        } else {
+            y = -(document.body.clientHeight / 2 - event.pageY);
+        }
+        console.log(event.pageY);
+        /*if (x > Math.abs(y)) {
+            console.log(x / y);
+        } else {
+            console.log(y / x);
+        }*/
+        let d = Math.atan(y / x) * (180 / Math.PI);
+        gun.style.transform = 'rotate(' + d + 'deg)';
+        deg.innerHTML = d + 'deg';
     }
-    else {
-        y = -(document.body.clientHeight / 2 - event.pageY);
-    }
-    let d = Math.atan(y / x) * (180 / Math.PI);
-    gun.style.transform = 'rotate(' + d + 'deg)';
-    deg.innerHTML = d + 'deg';
-})
+});
+
+
+
 
 
